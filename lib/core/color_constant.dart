e202c4b1873bcd790bf3ec9004f9c47e2b156840
MeAnandCB <anandcb55@gmail.com>
2024-05-09 16:57:24 +0530

    initial commit

---
+++ lib/core/color_constant.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color color1 = Color(0xFFF6F6F7);
+  static const Color color2 = Color(0xFF303954);
+  static const Color color3 = Color(0xFFBBD8AC);
+  static const Color color4 = Color(0xFF95A799);
+  static const Color color5 = Color(0xFFC3CFC7);
+  static const Color color6 = Color(0xFF7C7C91);
+  static const Color color7 = Color(0xFF7C9773);
+  static const Color color8 = Color(0xFFB4B4BF);
+  static const Color color9 = Color(0xFFF3C9C1);
+}
